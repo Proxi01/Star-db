4df5dc897e7039e88f4c7b3c19a6511000743ada
Rodion <r01proxi@gmail.com>
2019-05-15 11:47:48 -0400

    Added dynamically generating of person details component and spinner for this component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -34,7 +34,6 @@ export default class RandomPlanet extends Component {
     }
 
     updatePlanet = ()=>{
-        console.log('update');
         const id = Math.floor(Math.random() * 25) + 3;
         this.swapiService.getPlanet(id)
             .then(this.onPlanetLoaded)
